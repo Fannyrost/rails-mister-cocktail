8d6ae6409fe7ed5320a5492961905877b8acaf9a
Fanny Rostaing <fanny@coffretis.com>
2020-08-13 19:11:59 +0200

    finitions

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -2,8 +2,8 @@ import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
-    strings: ["Pimp your party", "Shake some cocktails !"],
-    typeSpeed: 50,
+    strings: ["Pimp your party ...", "Shake some cocktails !"],
+    typeSpeed: 40,
     loop: true
   });
 }
