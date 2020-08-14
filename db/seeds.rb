324e5b83e5827eacf86678f3bae88a412811fa53
Fanny Rostaing <fanny@coffretis.com>
2020-08-14 11:20:01 +0200

    seed changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,19 @@
 #   movies = Movie.create([{ name: 'Star Wars') { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-15.times do
-  Cocktail.create(name: Faker::Coffee.blend_name)
-end
 
-
-
-
-
-
+Cocktail.create(name: Faker::Coffee.blend_name, image_url:"https://image.flaticon.com/icons/svg/920/920711.svg")
+Cocktail.create(name: Faker::Coffee.blend_name, image_url:"https://image.flaticon.com/icons/svg/920/920719.svg")
+Cocktail.create(name: Faker::Coffee.blend_name, image_url:"https://image.flaticon.com/icons/svg/920/920728.svg")
+Cocktail.create(name: Faker::Coffee.blend_name, image_url:"https://image.flaticon.com/icons/svg/3254/3254550.svg")
+Cocktail.create(name: Faker::Coffee.blend_name, image_url:"https://image.flaticon.com/icons/svg/920/920706.svg")
+Cocktail.create(name: Faker::Coffee.blend_name, image_url:"https://image.flaticon.com/icons/svg/920/920729.svg")
+Cocktail.create(name: Faker::Coffee.blend_name, image_url:"https://image.flaticon.com/icons/svg/920/920697.svg")
+Cocktail.create(name: Faker::Coffee.blend_name, image_url:"https://image.flaticon.com/icons/svg/920/920584.svg")
+Cocktail.create(name: Faker::Coffee.blend_name, image_url:"https://image.flaticon.com/icons/svg/920/920577.svg")
+Cocktail.create(name: Faker::Coffee.blend_name, image_url:"https://image.flaticon.com/icons/svg/3126/3126698.svg")
+Cocktail.create(name: Faker::Coffee.blend_name, image_url:"https://image.flaticon.com/icons/svg/920/920700.svg")
+Cocktail.create(name: Faker::Coffee.blend_name, image_url:"https://image.flaticon.com/icons/svg/920/920702.svg")
+Cocktail.create(name: Faker::Coffee.blend_name, image_url:"https://image.flaticon.com/icons/svg/2093/2093423.svg")
+Cocktail.create(name: Faker::Coffee.blend_name, image_url:"https://image.flaticon.com/icons/svg/920/920577.svg")
 
